777b61df013ebeb2f0f2880c8689fa4f9af79ef9
Marcus Mesquita <marcus.mesquita@totvs.com.br>
2023-05-16 16:57:12 -0300

    Change who variables are passed to module

--- main.tf
+++ main.tf
@@ -1,44 +1,52 @@
 resource "aws_cloudfront_distribution" "cloudfront" {
-  origin {
-    domain_name = var.domain_name
-    origin_id   = var.origin_id
-  }
-
+  
   enabled             = var.cloudfront_enable
   default_root_object = var.default_root_object
+  aliases             = var.aliases
+  is_ipv6_enabled     = var.is_ipv6_enabled
 
-  aliases = var.aliases
-
+  custom_error_response {
+    error_caching_min_ttl = var.custom_error_response["error_caching_min_ttl"]
+    error_code            = var.custom_error_response["error_code"]
+    response_code         = var.custom_error_response["response_code"]
+    response_page_path    = var.custom_error_response["response_page_path"]
+  }
+  
   default_cache_behavior {
-    allowed_methods  = var.allowed_methods
-    cached_methods   = var.cached_methods
-    target_origin_id = var.target_origin_id
+    allowed_methods  = var.default_cache_behavior["allowed_methods"]
+    cached_methods   = var.default_cache_behavior["cached_methods"]
+    target_origin_id = var.default_cache_behavior["target_origin_id"]
+
+    compress               = var.default_cache_behavior["compress"]
+    viewer_protocol_policy = var.default_cache_behavior["viewer_protocol_policy"]
+    min_ttl                = var.default_cache_behavior["min_ttl"]
+    default_ttl            = var.default_cache_behavior["default_ttl"]
+    max_ttl                = var.default_cache_behavior["max_ttl"]
 
     forwarded_values {
-      query_string = false
-
+      query_string = var.default_cache_behavior["forwarded_values"]["query_string"]
       cookies {
-        forward = var.cookies_forward
+        forward = var.default_cache_behavior["forwarded_values"]["cookies"]["forward"]
       }
     }
-
-    viewer_protocol_policy = var.viewer_protocol_policy
-    min_ttl                = var.min_ttl
-    default_ttl            = var.default_ttl
-    max_ttl                = var.max_ttl
   }
+
+  origin {
+    domain_name = var.origin["domain_name"]
+    origin_id   = var.origin["origin_id"]
+  }
+  
   restrictions {
     geo_restriction {
-      restriction_type = var.restriction_type
-      locations        = var.restriction_locations
+      restriction_type = var.restrictions["geo_restriction"]["restriction_type"]
+      locations        = var.restrictions["geo_restriction"]["locations"]
     }
   }
 
   viewer_certificate {
-    cloudfront_default_certificate = var.cloudfront_default_certificate
-    acm_certificate_arn            = var.acm_certificate_arn
-    ssl_support_method             = var.ssl_support_method
-    minimum_protocol_version       = var.minimum_protocol_version
+    cloudfront_default_certificate = var.viewer_certificate["cloudfront_default_certificate"]
+    acm_certificate_arn            = var.viewer_certificate["acm_certificate_arn"]
+    ssl_support_method             = var.viewer_certificate["ssl_support_method"]
+    minimum_protocol_version       = var.viewer_certificate["minimum_protocol_version"]
   }
-
 }
--- variables.tf
+++ variables.tf
@@ -1,10 +1,6 @@
-variable "domain_name" {
+variable "origin" {
   description = ""
-}
-
-variable "origin_id" {
-  description = ""
-}
+} 
 
 variable "cloudfront_enable" {
   description = ""
@@ -18,58 +14,22 @@ variable "aliases" {
   description = ""
 }
 
-variable "allowed_methods" {
+variable "is_ipv6_enabled"{
   description = ""
 }
 
-variable "cached_methods" {
+variable "default_cache_behavior" {
   description = ""
 }
 
-variable "target_origin_id" {
+variable "restrictions" {
   description = ""
 }
 
-variable "cookies_forward" {
+variable "viewer_certificate" {
   description = ""
 }
 
-variable "viewer_protocol_policy" {
-  description = ""
-}
-
-variable "min_ttl" {
-  description = ""
-}
-
-variable "default_ttl" {
-  description = ""
-}
-
-variable "max_ttl" {
-  description = ""
-}
-
-variable "restriction_type" {
-  description = ""
-}
-
-variable "restriction_locations" {
-  description = ""
-}
-
-variable "cloudfront_default_certificate" {
-  description = ""
-}
-
-variable "acm_certificate_arn" {
-  description = ""
-}
-
-variable "ssl_support_method" {
-  description = ""
-}
-
-variable "minimum_protocol_version" {
+variable "custom_error_response" {
   description = ""
 }
